6bf839064fffd99ccbb580172084a15daff331ad
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-02 16:22:14 +1000

    Created seed.js file.

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+const userData = require("./userSeedData.json");
+const postData = require("./postSeedData.json");
+const commentData = require("./commentSeedData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
