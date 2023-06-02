18ad79c38c9af9229642c92de99bea52195a0296
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-03 07:54:40 +1000

    Created index.js for API routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,13 @@
 const router = require("express").Router();
+const loginApiRoutes = require("./login-api-routes.js");
+const userApiRoutes = require("./user-api-routes.js");
+const postApiRoutes = require("./post-api-routes.js");
+const commentApiRoutes = require("./comment-api-routes.js");
+
+// Set up the routes
+router.use("/login", loginApiRoutes);
+router.use("/user", userApiRoutes);
+router.use("/post", postApiRoutes);
+router.use("/comment", commentApiRoutes);
+
+module.exports = router;
