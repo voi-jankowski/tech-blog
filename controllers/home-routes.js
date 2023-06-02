f507343fa0be6f9a7438901ddc754a81fa46bd0b
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-02 17:33:13 +1000

    Added basic home-routes.

---
+++ controllers/home-routes.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+
+const { User, Post, Comment } = require("../models");
+
+// GET all posts for homepage
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("home", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET one post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    // Serialize the data
+    const post = postData.get({ plain: true });
+
+    // Pass serialized data and session flag into template
+    res.render("single-post", {
+      post,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
