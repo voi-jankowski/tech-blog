e7049cdaa6ee52c004bedd87021f98614e3718c5
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-04 19:01:00 +1000

    Created the test.html page with the dashboard and copied it to handlebars.

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,26 @@ $(".dropdown-trigger").dropdown();
 $(document).ready(function () {
   $(".collapsible").collapsible();
 });
+
+// Initialize fixed-action-btn
+$(".fixed-action-btn").floatingActionButton({
+  toolbarEnabled: true,
+});
+
+// Reinitialize all Materialize labels on the page if you are dynamically adding inputs:
+$(document).ready(function () {
+  M.updateTextFields();
+});
+
+// Prefill the textarea field
+
+$("#textarea1").val(
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+);
+M.textareaAutoResize($("#textarea1"));
+M.textareaAutoResize($("#create-post-textarea"));
+
+// Initialize modal
+$(document).ready(function () {
+  $(".modal").modal();
+});
