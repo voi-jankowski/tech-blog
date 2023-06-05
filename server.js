cffd4c3e9ae76abd0c08aa599c448d0e8ecc562a
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-06-05 13:12:19 +1000

    Created auth.js helper and made it available to handlebars.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routes = require("./controllers");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/connection");
 const crypto = require("crypto");
+const helpers = require("./utils/helper");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,14 +26,13 @@ const sess = {
   },
   resave: false,
   saveUninitialized: true,
-
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 app.use(session(sess));
 
